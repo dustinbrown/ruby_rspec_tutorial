8670dc33bd22a24fc055d782905df302ddee57e5
dustinbrown <dustinjamesbrown@gmail.com>
2014-03-19 23:31:25 -0700

    added comments for methods; fix save method with rescue incase @lib_file is not a path/string

--- library.rb
+++ library.rb
@@ -21,30 +21,36 @@ class Library
   end
 
   def get_books_in_category category
-        @books.select do |book|
-            book.category == category
-        end
+    #select books that match the category passed in and add to @books array
+    @books.select do |book|
+      book.category == category
+    end
   end
 
   def add_book book
+    #add passed in book to @books array
     @books.push book
   end
 
   def get_book title
+    #select book that title matches passed in title and add it to @books array
     @books.select do |book|
       book.title == title
     end.first
   end
 
   def save lib_file = false
-    #@lib_file = lib_file || @lib_file || "library.yml"
-    #if @lib_file.class != String
-      @lib_file = "library.yml"
-      #@lib_file = lib_file || "library.yml"
-    #end
+    #set @lib_file to passed in value or instance var or default string
+    @lib_file = lib_file || @lib_file || "library.yml"
+
+    #wrap file open block in case @lib_file is not a path/string
+    begin
       File.open @lib_file, "w" do |f|
-        #f.write @books.to_yaml
-        f.write YAML::dump @books
+        f.write @books.to_yaml
+        #f.write YAML::dump @books
       end
+    rescue Errno::ENOENT, /No such file or directory/
+      @lib_file = "library.yml"
+    end
   end
 end
